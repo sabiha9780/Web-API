9f7ceff3d10a07d0fc0f223fa41e75bfa1128933
SABIHA SULTANA <sabihasultanaafrin9780@gmail.com>
2025-04-16 01:29:38 +0600

    Add files via upload

---
+++ Controllers/SubjectController.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyAppData;
+using System.Drawing;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubjectController : ControllerBase
+    {
+        private readonly AppDbContext db;
+        private readonly ILogger<SubjectController> log;
+
+        public SubjectController(
+            AppDbContext ctx,
+            
+            ILogger<SubjectController> logger
+            )
+        {
+            db = ctx;
+            log = logger;
+        }
+
+        [HttpPost("/upload")]
+        public IActionResult Upload(IFormFile file,[FromServices] IWebHostEnvironment env)
+        {
+            try
+            {
+                if (file != null)
+                {
+                    string filePath = $"/images/{file.FileName}";
+                    string uploadPath = env.WebRootPath + filePath;
+                    using var stream = System.IO.File.Create(uploadPath);
+                    file.CopyTo(stream);
+                    return Ok(filePath);
+                }
+                return BadRequest();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            var data = db.Subjects.Include(a => a.Students).ToList();
+            log.LogInformation("data retrieved success");
+            return Ok(data);
+
+
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            try
+            {
+                var data = db.Subjects.Include(a => a.Students).FirstOrDefault(a => a.SubId == id);
+                log.LogInformation("data retrieved success");
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Save(Subject sub)
+        {
+            try
+            {
+                //if (sub.ExamDate.Date > DateTime.Today.Date)
+                //{
+                //    ModelState.AddModelError("ExamDate", "invalid exam date");
+
+                //}
+                //if (sub.Students is not null && sub.Students.Any(s => s.Marks > sub.TotalMarks))
+                //{
+                //    ModelState.AddModelError("", "invalid marks");
+                //}
+
+                if (ModelState.IsValid)
+                {
+                    db.Subjects.Add(sub);
+                    db.SaveChanges();
+                    log.LogInformation("data insert success");
+                    return Ok(sub);
+                }
+                else
+                {
+                    return BadRequest(ModelState);
+                }
+            }
+            catch
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Update(Subject sub)
+        {
+            try
+            {
+                //if (sub.ExamDate.Date > DateTime.Today.Date)
+                //{
+                //    ModelState.AddModelError("ExamDate", "invalid exam date");
+
+                //}
+                //if (sub.Students is not null && sub.Students.Any(s => s.Marks > sub.TotalMarks))
+                //{
+                //    ModelState.AddModelError("", "invalid marks");
+                //}
+                if (ModelState.IsValid)
+                {
+                    var oldData = db.Subjects.Include(a => a.Students).First(a => a.SubId == sub.SubId);
+                    db.Subjects.Remove(oldData);
+
+                    db.Subjects.Add(sub);
+                    db.SaveChanges();
+                    log.LogInformation("data update success");
+                    return Ok(sub);
+                }
+                else
+                {
+                    return BadRequest(ModelState);
+                }
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                var data = db.Subjects.Include(a => a.Students).FirstOrDefault(a => a.SubId == id);
+                if (data == null)
+                {
+                    return NotFound($"Data not found by subject id {id}");
+                }
+                db.Subjects.Remove(data);
+                db.SaveChanges();
+                log.LogTrace("data delete success");
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyAppData;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly AppDbContext db;
+
+        public ValuesController(AppDbContext ctx)
+        {
+            db = ctx;
+        }
+        [HttpGet("/Max")]
+        public IActionResult GetMax()
+        {
+            var marks = db.Students.Max(a => a.Marks);
+            return Ok(marks);
+        }
+        [HttpGet("/Min")]
+        public IActionResult GetMin()
+        {
+            var marks = db.Students.Min(a => a.Marks);
+            return Ok(marks);
+        }
+
+
+        [HttpGet("/SubjectHighest")]
+        public IActionResult GetSubMax()
+        {
+            try
+            {
+                var data = db.Students.Include(s => s.Subject).
+               Select(s => new { Name = s.Name, Marks = s.Marks, SubName = s.Subject.SubName });
+
+
+                var grpData = data.GroupBy(a => a.SubName).Select(s => new { HighestMarks = s.Max(a => a.Marks), SubjectName = s.Key });
+
+
+
+                return Ok(grpData);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e);
+            }
+
+        }
+        [HttpGet("/StudentHighest")]
+        public IActionResult GetStudentMax()
+        {
+            try
+            {
+                var data = db.Students.GroupBy(a => a.Name).Select(s => new { HighestMarks = s.Max(a => a.Marks), StudentName = s.Key });
+
+                return Ok(data);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e);
+            }
+
+        }
+        [HttpGet("/StudentTotal")]
+        public IActionResult GetStudentTotal()
+        {
+            try
+            {
+                var data = db.Students.GroupBy(a => a.Name).Select(s => new { Total = s.Sum(a => a.Marks), StudentName = s.Key });
+
+                return Ok(data);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e);
+            }
+
+        }
+        [HttpGet("/StudentExamCount")]
+        public IActionResult GetStudentExamCount()
+        {
+
+            var data = db.Students.GroupBy(a => a.Name).Select(s => new { Count = s.Count(), StudentName = s.Key });
+
+            return Ok(data);
+
+
+        }
+        [HttpGet("/StudentMin")]
+        public IActionResult GetStudentMin()
+        {
+
+            var data = db.Students.GroupBy(a => a.Name).Select(s => new { Minimum = s.Min(a => a.Marks), StudentName = s.Key });
+
+            return Ok(data);
+
+
+        }
+        [HttpGet("/StudentAvg")]
+        public IActionResult GetStudentAverage()
+        {
+
+            var data = db.Students.GroupBy(a => a.Name).Select(s => new { Average = s.Average(a => a.Marks), StudentName = s.Key });
+
+            return Ok(data);
+
+        }
+    }
+}
